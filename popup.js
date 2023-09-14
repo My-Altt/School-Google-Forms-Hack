7629b707c7101169d06439482278f5a669c66d6c
My-Altt <101564533+My-Altt@users.noreply.github.com>
2023-09-14 18:56:33 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,23 +1,61 @@
-document.getElementById("submitBtn").addEventListener("click", () => {
-  // Define the URL of the Google Form you want to automate.
-  const googleFormUrl = "https://docs.google.com/forms/d/e/1FAIpQLScO9RFv3JT6GAoQ7fBzyy5JoVE65GX8a1s9vB4ZZ19Xa4p7Dg/viewform";
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { OpenAIApi } = require('openai');
 
-  // Define the answer you want to submit (replace "your_answer" with the actual answer).
-  const answer = "your_answer";
+// Google Forms URL
+const googleFormUrl = 'https://docs.google.com/forms/your_form_id_here/viewform';
 
-  // Open a new tab to the Google Form and fill in the answer.
-  chrome.tabs.create({ url: googleFormUrl }, (tab) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: (answer) => {
-        // Locate the multiple-choice question element and select the answer.
-        const question = document.querySelector("your_question_selector_here");
-        question.value = answer;
-        
-        // Submit the form.
-        document.querySelector("form").submit();
-      },
-      args: [answer],
-    });
+// ChatGPT API Key
+const apiKey = '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';
+const openai = new OpenAIApi({ key: apiKey });
+
+// Function to extract form data from Google Forms
+async function extractFormData(formUrl) {
+  const response = await axios.get(formUrl);
+  const $ = cheerio.load(response.data);
+
+  // Extract questions and options here.
+  // You'll need to inspect the HTML structure of the form and use CSS selectors to extract data.
+}
+
+// Function to ask ChatGPT a question
+async function askChatGPT(question) {
+  const response = await openai.createCompletion({
+    prompt: question,
+    max_tokens: 50, // Adjust as needed
   });
-});
+
+  return response.choices[0].text;
+}
+
+// Function to complete the Google Form
+async function completeGoogleForm(formData) {
+  // Use Puppeteer or another tool to automate form completion.
+  // This part would be quite complex and specific to the form's structure.
+}
+
+// Main function
+async function automateGoogleForm() {
+  try {
+    // Step 1: Extract form data
+    const formData = await extractFormData(googleFormUrl);
+
+    // Step 2: Ask ChatGPT for answers
+    const answers = [];
+    for (const question of formData.questions) {
+      const answer = await askChatGPT(question);
+      answers.push(answer);
+    }
+
+    // Step 3: Complete the Google Form
+    await completeGoogleForm({ questions: formData.questions, answers });
+
+    // Step 4: Send the completed form back to the user
+    console.log('Completed form:', { questions: formData.questions, answers });
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+// Run the automation
+automateGoogleForm();
