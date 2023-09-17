37831156115489ea3cb9514336ca094016c52088
My-Altt <101564533+My-Altt@users.noreply.github.com>
2023-09-17 16:02:34 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,61 +1,16 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const { OpenAIApi } = require('openai');
-
-// Google Forms URL
-const googleFormUrl = 'https://docs.google.com/forms/your_form_id_here/viewform';
-
-// ChatGPT API Key
-const apiKey = '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';
-const openai = new OpenAIApi({ key: apiKey });
-
-// Function to extract form data from Google Forms
-async function extractFormData(formUrl) {
-  const response = await axios.get(formUrl);
-  const $ = cheerio.load(response.data);
-
-  // Extract questions and options here.
-  // You'll need to inspect the HTML structure of the form and use CSS selectors to extract data.
-}
-
-// Function to ask ChatGPT a question
-async function askChatGPT(question) {
-  const response = await openai.createCompletion({
-    prompt: question,
-    max_tokens: 50, // Adjust as needed
-  });
-
-  return response.choices[0].text;
-}
-
-// Function to complete the Google Form
-async function completeGoogleForm(formData) {
-  // Use Puppeteer or another tool to automate form completion.
-  // This part would be quite complex and specific to the form's structure.
-}
-
-// Main function
-async function automateGoogleForm() {
-  try {
-    // Step 1: Extract form data
-    const formData = await extractFormData(googleFormUrl);
-
-    // Step 2: Ask ChatGPT for answers
-    const answers = [];
-    for (const question of formData.questions) {
-      const answer = await askChatGPT(question);
-      answers.push(answer);
+// This code runs when the popup.html is opened
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  // Send a message to the content script to extract questions
+  chrome.tabs.sendMessage(tabs[0].id, { action: "extractQuestions" }, function (response) {
+    const questionList = document.getElementById("questionList");
+    if (response && response.questions) {
+      response.questions.forEach(function (question) {
+        const listItem = document.createElement("li");
+        listItem.textContent = question;
+        questionList.appendChild(listItem);
+      });
+    } else {
+      questionList.innerHTML = "No questions found on this page.";
     }
-
-    // Step 3: Complete the Google Form
-    await completeGoogleForm({ questions: formData.questions, answers });
-
-    // Step 4: Send the completed form back to the user
-    console.log('Completed form:', { questions: formData.questions, answers });
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
-
-// Run the automation
-automateGoogleForm();
+  });
+});
