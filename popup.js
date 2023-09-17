04a9819d9f873a953b806f9764cd080af3885bf6
My-Altt <101564533+My-Altt@users.noreply.github.com>
2023-09-17 16:25:38 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,41 @@
-// Get the questions from the background script and display them
-chrome.runtime.getBackgroundPage((backgroundPage) => {
-  const questions = backgroundPage.questions;
-  const questionsDiv = document.getElementById('questions');
-  questionsDiv.innerHTML = '';
-  questions.forEach((question, index) => {
-    const questionElement = document.createElement('div');
-    questionElement.textContent = `Question ${index + 1}: ${question}`;
-    questionsDiv.appendChild(questionElement);
-  });
-});
+function attachEventListeners() {
+    let startQuesRadio = document.getElementById('startQues');
+    startQuesRadio.addEventListener('click', () => startQuesRadio.select());
+
+    document.getElementById('endQuesRadio').addEventListener('click', () => {
+        let lastQuesRadio = document.getElementById('lastQuesRadio');
+        lastQuesRadio.checked = false;
+        let fieldText = document.getElementById('endQuesField');
+        fieldText.select();
+    });
+
+    let textField = document.getElementById('endQuesField');
+    textField.addEventListener('click', () => {
+        let lastQuesRadio = document.getElementById('lastQuesRadio');
+        lastQuesRadio.checked = false;
+        let endQuesRadio = document.getElementById('endQuesRadio');
+        endQuesRadio.checked = true;
+        textField.select();
+    });
+
+    document.getElementById('lastQuesRadio').addEventListener('click', () => {
+        let endQuesRadio = document.getElementById('endQuesRadio');
+        endQuesRadio.checked = false;
+    })
+}
+
+attachEventListeners();
+
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function (event, activeTab) {
+    try {
+        const data = new FormData(form);
+        const startQues = parseInt(data.get("startQues"));
+        const endQues = parseInt(data.get("endQues"));
+        const lastQues = data.get("lastQues");
+
+        if (Math.min(startQues, endQues) < 1 || startQues > endQues) {
+            throw new Error('Invalid range');
+        }
+}, 
